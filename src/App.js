853d8ae45f9691e3dfd793c3ab93158b11ef23f3
Sooraj Chandran <soorajchn@gmail.com>
2018-06-12 22:13:25 +0200

    🎉 Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+import './App.css'
+
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div>as</div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => (
+  <div className="tmj-header">
+    <h2>
+      <a href="/">Track my jobs</a>
+    </h2>
+  </div>
+)
+
+export default Header
