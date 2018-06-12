74da80bb27ba53ebab4819af3cf48846a9b43cdf
Sooraj Chandran <soorajchn@gmail.com>
2018-06-12 22:26:31 +0200

    💄 Change header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,7 @@ import './Header.css'
 
 const Header = () => (
   <div className="tmj-header">
-    <h2>
-      <a href="/">Track my jobs</a>
-    </h2>
+    <a href="/">Track my jobs</a>
   </div>
 )
 
