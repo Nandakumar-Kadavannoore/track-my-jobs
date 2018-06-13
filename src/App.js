dfc3077bd6505cad4a4037a957ea2f048217f3d8
Sooraj Chandran <soorajchn@gmail.com>
2018-06-13 21:41:45 +0200

    ✨ Add trello like interface for sections

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react'
+import Board from 'react-trello'
+
 import Header from './components/Header'
 import './App.css'
+
+const CustomCard = props => (
+  <React.Fragment>
+    <div className="card">{props.title}</div>
+  </React.Fragment>
+)
 
 class App extends Component {
   state = {}
   render() {
+    const data = {
+      lanes: [
+        {
+          id: 'lane1',
+          title: 'Applied/Awaiting response',
+          label: '2/2',
+          cards: [
+            { id: 'Card1', title: 'Google', description: 'Software Engineer', label: '30 mins' },
+            { id: 'Card2', title: 'Facebook', description: 'Software engineer', label: '5 mins' },
+          ],
+        },
+        { id: 'lane2', title: 'Received First response', label: '0/0', cards: [] },
+        { id: 'lane3', title: 'Interview scheduled', label: '0/0', cards: [] },
+        { id: 'lane4', title: 'Second Round of interview', label: '0/0', cards: [] },
+        { id: 'lane5', title: 'Recieved offer', label: '0/0', cards: [] },
+        { id: 'lane5', title: 'Rejected / Declined', label: '0/0', cards: [] },
+      ],
+    }
     return (
       <React.Fragment>
         <Header />
-        <div>Hello world</div>
+        <Board
+          data={data}
+          draggable
+          laneDraggable={false}
+          style={{ background: 'white' }}
+          editable
+          customCardLayout
+        >
+          <CustomCard />
+        </Board>
       </React.Fragment>
     )
   }
