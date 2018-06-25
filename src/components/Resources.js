d9811698c5e336b098ce39bc937391c2f62ddacd
Sooraj Chandran <soorajchn@gmail.com>
2018-06-25 22:38:43 +0200

    ✨ Add resources section

---
+++ src/components/Resources.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './components.css'
+
+const Resources = () => (
+  <div className="job-board">
+    Resources
+    <span> resource </span>
+  </div>
+)
+
+export default Resources
