6916850c43d4341e7a4a8cbb7f14cb51666dd323
Sooraj Chandran <soorajchn@gmail.com>
2018-07-01 08:36:09 +0200

    💄 Add basic design for job board

--- src/components/JobBoard.js
+++ src/components/JobBoard.js
@@ -3,14 +3,21 @@ import React from 'react'
 import './components.css'
 
 const JobBoard = ({ jobs }) => (
-  <div className="job-board">
-    Job Board
-    {jobs.map(job => (
-      <div>
-        <span> {job.name} </span>
-        <span> {job.description} </span>
-      </div>
-    ))}
+  <div className="container">
+    <h2> Jobs </h2>
+    <div className="job-board row">
+      {jobs.map(job => (
+        <div className="col-sm-3">
+          <div className="job-card">
+            <div className="job-title"> {job.name} </div>
+            <div> {job.description} </div>
+            <div>
+              <a href={job.url}> View job </a>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 )
 
