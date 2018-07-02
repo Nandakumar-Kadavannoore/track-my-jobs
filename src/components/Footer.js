49905eefd9ad5bd4da92c1be04792bf70eb80e6f
Keerthi Jayadevan K <keerthi.jayadevan@gmail.com>
2018-07-02 21:47:49 +0530

    💄  Add footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import './components.css'
 
 const Footer = () => (
   <div className="tmj-footer">
-    <span>something</span>
+    <span>Made with ❤️ by <a href="https://twitter.com/iamkeerthij">Keerthi</a> and <a href="https://twitter.com/iamsooraj"> Sooraj </a></span>
   </div>
 )
 
