537f00408b0a50d1083a084219c11b2c4dabd591
Sooraj Chandran <soorajchn@gmail.com>
2018-07-08 09:08:37 +0200

    🚨 Make footer prettier

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import './components.css'
 
 const Footer = () => (
   <div className="tmj-footer">
-    <span>Made with ❤️ by <a href="https://twitter.com/iamkeerthij">Keerthi</a> and <a href="https://twitter.com/iamsooraj"> Sooraj </a></span>
+    <span>
+      Made with ❤️ by <a href="https://twitter.com/iamkeerthij">Keerthi</a> and
+      <a href="https://twitter.com/iamsooraj"> Sooraj </a>
+    </span>
   </div>
 )
 
